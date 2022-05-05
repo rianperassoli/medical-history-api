c795caa2951dfd32c8763570ad4e31e8b9334765
Rian Perassoli <rian.perassoli@gmail.com>
2022-05-05 20:05:00 -0300

    refactoring of method Get users

---
+++ src/DTOs/IResponseUserDTO.ts
@@ -0,0 +1,9 @@
+interface IResponseUserDTO {
+  fullname: string;
+  gender: string;
+  email: string;
+  birthdate: Date;
+  created_at: Date;
+}
+
+export { IResponseUserDTO };
--- src/services/ListAllUsersService.ts
+++ src/services/ListAllUsersService.ts
@@ -1,11 +1,24 @@
-import { User } from "../entities/User";
+import { IResponseUserDTO } from "../DTOs/IResponseUserDTO";
 import { UserRepository } from "../repositories/UserRepository";
 
 class ListAllUsersService {
-  async execute(): Promise<User[]> {
+  async execute(): Promise<IResponseUserDTO[]> {
     const userRepository = new UserRepository()
 
-    return userRepository.findAll();
+    const users = await userRepository.findAll();
+
+    const usersToReturn: IResponseUserDTO[] = users.map(user => {
+      const fullname = `${user.first_name} ${user.last_name}`
+
+      delete user.id
+      delete user.first_name
+      delete user.last_name
+      delete user.phone
+
+      return { ...user, fullname }
+    })
+
+    return usersToReturn
   }
 }
 
