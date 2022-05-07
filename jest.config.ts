8407eb170689c59cc0ca09261c51fb987b93878a
Rian Perassoli <rian.perassoli@gmail.com>
2022-05-07 18:21:00 -0300

    Added controllers to coverage

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,7 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["<rootDir>/src/services/*.ts"],
+  collectCoverageFrom: ["<rootDir>/src/services/*.ts", "<rootDir>/src/controllers/*.ts"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
